9151764ec6d436d8dc6ac2b7e3596a667072ee44
Lê Minh Quý <92782164+QuyLe-Minh@users.noreply.github.com>
2023-04-19 12:52:48 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -25,6 +25,7 @@ int main() {
     string s1, s2;
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     for (int i = 0; i < 100; i++){
+        //  EDIT HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         string expect = "C:\\Users\\Admin\\OneDrive\\ass2\\testcase\\output\\expected" + to_string(i+1) + ".txt";       //<-------- Throw your output folder path here and add additional \ in your path. Then add \\expected. In general, your path will look similar mine.
         string got = "got" + to_string(i+1) + ".txt";
 
