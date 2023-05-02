8b966bb2d3bc7665170ee3ab5027b55e15ef3788
Lê Minh Quý <92782164+QuyLe-Minh@users.noreply.github.com>
2023-05-02 09:55:31 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
     for (int tc = 1; tc <= 200; tc++){
         //  EDIT HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-        string fileName = "C:\\Users\\Admin\\OneDrive\\ass2\\testcase\\input\\input" + to_string(tc) + ".txt";  //<-------- Throw your output folder path here and add additional \ in your path. Then add \\expected. In general, your path will look similar mine.
+        string fileName = "C:\\Users\\Admin\\OneDrive\\ass2\\testcase\\input\\input" + to_string(tc) + ".txt";  //<-------- Throw your output folder path here and add additional \ in your path. Then add \\input. In general, your path will look similar mine.
         simulate(fileName, tc);
     }
 
